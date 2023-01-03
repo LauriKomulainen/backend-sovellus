948ce85f6402695844d609437e07d0714ef1047d
LauriKomulainen <lakom94@gmail.com>
2023-01-03 11:37:47 +0200

    Add rng

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ app.use(express.static("public"));
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.get("/randomize", (req, res) => {
+  const randomNumbers = [];
+  for (let i = 0; i < 3; i++) {
+    randomNumbers.push(Math.floor(Math.random() * 3) + 1);
+  }
+  let message = "<table>";
+
+  for (let i = 0; i < randomNumbers.length; i++) {
+    message += `<tr><td>${randomNumbers[i]}</td></tr>`;
+  }
+  message += "</table>";
+  if (
+    randomNumbers[0] === randomNumbers[1] &&
+    randomNumbers[1] === randomNumbers[2]
+  ) {
+    res.send(`Random numbers: ${message} Success`);
+  }
+  res.send(`Random numbers: ${message}`);
+});
